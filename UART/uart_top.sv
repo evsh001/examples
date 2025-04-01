f17bd970b7e6c1bc1a5a4944010c67914621b1ac
evsh001 <158470874+evsh001@users.noreply.github.com>
2025-04-01 23:47:29 +0300

    Update uart_top.sv

--- UART/uart_top.sv
+++ UART/uart_top.sv
@@ -1,31 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
 
 module uart_top
-  #
-  (
-  parameter RAM_SIZE     = 16,
-  parameter CLK_FREQ     = 44,
-  )
   (
   input wire          clk, // 44Mhz clock
+  input wire [7:0]    SW,
+  output wire         tx_out,
+  output wire         LED
+  );
 
-  // Pushbutton interface
-  input logic         BTNTX,
-  //input logic         BTNRX,
-
-  // UART pins
-  // output wire         rx_in,
-  output wire         tx_out
-);
-
-  logic [7:0]                  ram_store[RAM_SIZE];
-  logic [$clog2(RAM_SIZE)-1:0] ram_rdaddr;
-  logic [7:0]                  ram_dout;
+  logic [24:0]                 counter;
+  logic                        start;
+  logic                        clk_en;
+  logic       	               done;
 
 
+  uart_clk_gen u_uart_clk_gen
+  (
+   .clk            (clk),
+   .clk_en         (clk_en)
+   );
+   
+   tx_block u_tx_block
+  (
+   .clk            (clk),
+   .start          (start),
+   .mdata          (SW),
+   .clk_en         (clk_en),
+   .done           (done),
+   .tx_out         (tx_out)
+   );
+  
   always @(posedge clk) begin
-    ram_dout <= ram_store[ram_rdaddr];
+    start <= 0;
+	if (&counter)
+      start <= 1'b1;
+    else
+      counter <= counter + 1;
   end
-
-
+  
+  assign LED = done;
 
 endmodule
