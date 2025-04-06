a71e6adf052f148d58d6edc8e36e373c8da180e1
evsh001 <158470874+evsh001@users.noreply.github.com>
2025-04-06 21:01:25 +0300

    Update tx_block.sv

--- UART/tx_block.sv
+++ UART/tx_block.sv
@@ -8,31 +8,37 @@ module tx_block(
   input  logic [7:0] mdata,
   input  logic       clk_en,
   output logic       done,
-  output wire        tx_out
+  output logic       tx_out
  );
 
   localparam CLK_COUNT = 10;
 
   logic [9:0]   txdata;
   logic         tx;
-  logic [$clog2(CLK_COUNT)-1:0]  counter;
+  logic [$clog2(CLK_COUNT)-1:0]  index_counter;
 
-  always_ff @( posedge clk ) begin
+  initial begin 
+    txdata  = '0;
+    tx      = '0;
+    index_counter = '0;
+    tx_out  = 1'b1;
+  end
+
+  always @(posedge clk) begin
     done        <= '0;
     if (start) begin
       tx        <= '1;
-      txdata    <= {1'b1, mdata, '0};   //  stop bit + mdata + start bit
-      counter   <= '0;
+      txdata    <= {1'b1, mdata, '0};   //  stop bit + data + start bit
     end 
-    if (tx && clk_en && counter < CLK_COUNT) begin
+    if (tx && clk_en && index_counter < CLK_COUNT) begin
+      tx_out <= txdata[0];
       txdata    <= txdata >> 1;
-      counter   <= counter + 1'b1;
-    end else if (counter == CLK_COUNT) begin
+      index_counter   <= index_counter + 1'b1;
+    end else if (clk_en && index_counter == CLK_COUNT) begin
       done      <= 1'b1;
       tx        <= '0;
+      index_counter   <= '0;
     end
   end
-
-  assign tx_out = tx ? txdata[0] : 1'b1;  
-
+    
 endmodule
