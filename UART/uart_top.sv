85cc8542790d0305badddabc8fcbecb8a183f24e
evsh001 <158470874+evsh001@users.noreply.github.com>
2025-04-06 21:02:46 +0300

    Update uart_top.sv

--- UART/uart_top.sv
+++ UART/uart_top.sv
@@ -3,17 +3,24 @@
 
 module uart_top
   (
-  input wire          clk, // 44Mhz clock
-  input wire [7:0]    SW,
-  output wire         tx_out,
-  output wire         LED
+  input wire           clk, // 44Mhz clock
+  input wire [7:0]     SW,
+  output logic         tx_out
   );
 
-  logic [24:0]                 counter;
+  logic [22:0]                 counter_st;
+  //logic [6:0]                  counter_st;   // TESTBENCH
   logic                        start;
   logic                        clk_en;
   logic       	               done;
 
+
+  initial begin
+    counter_st = '0;
+    start   = '0;
+    clk_en  = '0;
+    done    = '0;
+  end
 
   uart_clk_gen u_uart_clk_gen
   (
@@ -33,12 +40,11 @@ module uart_top
   
   always @(posedge clk) begin
     start <= 0;
-    if (&counter)
-      start <= 1'b1;
-    else
-      counter <= counter + 1;
+	if (&counter_st) begin
+      start         <= 1'b1;
+      counter_st    <= '0;     
+    end else
+      counter_st <= counter_st + 1;
   end
-  
-  assign LED = done;
-
+ 
 endmodule
