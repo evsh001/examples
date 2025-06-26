1a266d8dad19a644b362e82dbf2618a68b95c55f
evsh001 <158470874+evsh001@users.noreply.github.com>
2025-06-26 23:09:34 +0300

    Update tx_block.sv

--- UART/tx_block.sv
+++ UART/tx_block.sv
@@ -1,44 +1,69 @@
-
-`timescale 1ns / 1ps
+`timescale 1ns / 10ps
 //////////////////////////////////////////////////////////////////////////////////
 
-module tx_block(
+module uart_tx_block
+ #
+ (
+  parameter          CLK_FREQ      = 44_000_000,     // Hz
+  parameter          BAUD_RATE     = 230_400         // Bod
+ )
+ (
   input  wire        clk,
-  input  logic       start,
-  input  logic [7:0] mdata,
-  input  logic       clk_en,
-  output logic       done,
+  input  logic [7:0] data,
+  input  logic       tx_start,
+  output logic       tx_done,
+  output logic       tx_busy,
   output logic       tx_out
  );
 
-  localparam CLK_COUNT = 10;
+  localparam DATA_LEN  = 10;
+  localparam CLK_COUNT = int'(CLK_FREQ/BAUD_RATE + 1);  // 190 + 1 that is round (depending on the input frequency)
+  
+//  logic                              tx_busy;
+  logic                              clk_en;
+  logic [DATA_LEN-1:0]               tx_data;
+  logic [$clog2(DATA_LEN)-1:0]       index;
+  logic [$clog2(CLK_COUNT)-1:0]      clk_en_cnt;
+  
 
-  logic [9:0]   txdata;
-  logic         tx;
-  logic [$clog2(CLK_COUNT)-1:0]  index_counter;
-
-  initial begin 
-    txdata  = '0;
-    tx      = '0;
-    index_counter = '0;
-    tx_out  = 1'b1;
+  initial begin
+    // clock gen variables
+    clk_en        = '0; 
+    clk_en_cnt    = '0;
+    // uart tx block  variables
+    tx_data       = '0;
+    tx_busy       = '0;
+    index         = '0;
+    tx_out        = 1'b1;
+    tx_done       = '0;   
   end
 
+
   always @(posedge clk) begin
-    done        <= '0;
-    if (start) begin
-      tx        <= '1;
-      txdata    <= {1'b1, mdata, '0};   //  stop bit + data + start bit
+    clk_en        <= '0;
+    if (clk_en_cnt == CLK_COUNT - 1 || tx_start) begin
+      clk_en_cnt <= '0;
+      clk_en    <= ~clk_en;
+    end else
+      clk_en_cnt <= clk_en_cnt + 1;
+  end
+  
+  always @(posedge clk) begin
+    tx_done        <= '0;
+    if (tx_start && ~tx_busy) begin
+      tx_busy   <= '1;
+      tx_data   <= {1'b1, data, '0};   //  stop bit + data[MSB...LSB] + start bit
     end 
-    if (tx && clk_en && index_counter < CLK_COUNT) begin
-      tx_out <= txdata[0];
-      txdata    <= txdata >> 1;
-      index_counter   <= index_counter + 1'b1;
-    end else if (clk_en && index_counter == CLK_COUNT) begin
-      done      <= 1'b1;
-      tx        <= '0;
-      index_counter   <= '0;
+    if (tx_busy && clk_en && index < DATA_LEN) begin
+      tx_out    <= tx_data[0];
+      tx_data   <= tx_data >> 1;
+      index     <= index + 1'b1;
+    end else if (clk_en && index == DATA_LEN) begin
+      tx_done   <= 1'b1;
+      tx_busy   <= '0;
+      index     <= '0;
     end
   end
     
+
 endmodule
